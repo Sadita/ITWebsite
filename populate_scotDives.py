eb3b8e2ebfe2e457aab5ef973a464914b1b3ad47
Rebecca McGinn <rebeccamcginn2014@icloud.com>
2018-03-14 12:51:14 +0000

    added text to populate_scotDives

--- populate_scotDives.py
+++ populate_scotDives.py
@@ -20,17 +20,21 @@ def populate():
                                                  "Level: All levels, suitable for training beginners \n\nDepth: 15m "
                                                  "\n\nAvailable Parking: On site in front of first entry point",
                                   "image": "images/fifeness.jpg"},
-                    "Isle of Man": {"rating": 2.5, "latitude": 56.174592, "longitude": -5.083394,
-                                    "description": "To reach the Isle of Man, you can either take your own boat and "
+                    "Isle of May": {"rating": 2.5, "latitude": 56.174592, "longitude": -5.083394,
+                                    "description": "To reach the May Isle, you can either take your own boat and "
                                     			"dock at one of the two inlets, or hop on the May Princess ferry for "
                                     			"a slower transfer. The Isle is home to hundreds of puffins who will "
                                     			"pierce the water around you as you descend. This is an excellent "
                                     			"location if you want to try diving with seals. The resident seals "
                                     			"will enjoy nipping at your fins when you move further out from the "
-                                    			"Isle. Depth > 40 metres", "image": "images/isleofman.jpg"},
+                                    			"Isle. \n\nDifficulty Level: ??????????????????????? \n\nDepth: >40m "
+                                                 "\n\nAvailable Parking: Parking available at harbour in Anstruther",
+                                                 "image": "images/isleofman.jpg"},
                     "Kentallen Wall": {"rating": 3, "latitude": 56.385906, "longitude": -2.974244,
-                                       "description": "a descritopn  for Kentallen Wall",
-                                       "image": "images/kentallenwall.jpg"},
+                                       "description": "a descritopn  for Kentallen Wall\n\nDifficulty Level: "
+                                                "??????????????????????? \n\nDepth: ?????????m"
+                                                 "\n\nAvailable Parking: In the Holly Tree carpark in front of the pier",
+                                                "image": "images/kentallenwall.jpg"},
                     "The Slates (Ballachulish)": {"rating": 2, "latitude": 55.432373, "longitude": -3.156804,
                                                   "description": "There are several entry points here. From the parking "
                                                   "area, you can walk straight towards the loch and down the slope into "
@@ -38,8 +42,9 @@ def populate():
                                                   "lack of current. For the second entry point, walk along the grassy "
                                                   "path leading from the parking area until you reach a sandy slope down "
                                                   "to the water. This entry point quickly leads to a boulder reef at "
-                                                  "around 20m depth. ",
-                                                  "image": "images/theslates.jpg"},
+                                                  "around 20m depth.\n\nDifficulty Level: ??????????????????????? \n\nDepth: "
+                                                 "\n\nAvailable Parking: On site in front of first entry point",
+                                                 "image": "images/isleofman.jpg"},
                     "SS Afton": {"rating": 3.5, "latitude": 55.341912, "longitude": -4.733811,
                                  "description": "a descritopn  for SS Afton", "image": "images/ssafton.jpg"}
                     }
