82cc2a607a12eef9bf556b2c2cd7ebded7f31304
Sadita Ahmed <2304007a@student.gla.ac.uk>
2018-03-18 05:38:10 +0000

    star-rating & comments

--- scotDives/forms.py
+++ scotDives/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from scotDives.models import UserProfile
+from scotDives.models import UserProfile, Review
 from registration.forms import RegistrationForm
         
 class UserForm(RegistrationForm):
@@ -12,8 +12,20 @@ class UserForm(RegistrationForm):
         model = User
         fields = ('first_name', 'last_name', 'username', 'email')
 
+
 class UserProfileForm(forms.ModelForm):
     picture = forms.ImageField(required=False)
     class Meta:
         model = UserProfile
         fields = ('picture',)
+
+
+class UserReviewForm(forms.ModelForm):
+    rating = forms.IntegerField(required=False)
+    comment = forms.CharField(max_length=1000, required=False)
+
+    class Meta:
+        model = Review
+        fields = ('rating', 'comment')
+
+
